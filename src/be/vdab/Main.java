e93fce5aafa259b17a4ddef5b4eb991721eb2794
Gust Van de Wouwer <gust.vandewouwer@gmail.com>
2017-08-29 15:37:53 +0200

    hello everybody

--- src/be/vdab/Main.java
+++ src/be/vdab/Main.java
@@ -4,9 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		System.out.println("Hallo allemaal");
-		
+		System.out.println("Hello everybody");
 	}
 
 }
